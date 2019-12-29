0b1418d5ed844723ae6d464250a68db9bc92cda4
likebr2000 <54508405+likebr2000@users.noreply.github.com>
2019-12-29 17:53:17 +0900

    Update dday-result.js

--- 06/js/dday-result.js
+++ 06/js/dday-result.js
@@ -6,7 +6,7 @@ var passedTime = toNow - toFirst;  // 처음 만난 날과 오늘 사이의 차�
 var passedDay = Math.round(passedTime/(24*60*60*1000)); // 밀리초를 일로 변환 후 반올림합니다.
 document.querySelector('#accent').innerText = passedDay + "일";  // #accent 영역에 표시합니다.
 
-calcDate(100);  // 100일 기념일을 계산해서 표시합니다.
+calcDate(100);  // 100일 기념일을 계산해서ㄹㅇ 표시합니다.
 calcDate(300);  // 200일 기념일을 계산해서 표시합니다.
 calcDate(365);  // 1년 기념일을 계산해서 표시합니다.
 calcDate(500);  // 500일 기념일을 계산해서 표시합니다.
